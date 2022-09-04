be5e096edce7063f06d24d069751a1604699be4b
Victtor De Souza Rodrigues <60055607+Victtor-Souza@users.noreply.github.com>
2022-09-04 18:35:05 -0300

    feat: implementando autenticacao com jwt

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+namespace Blog;
+
+public static class Configuration 
+{
+    public static string JwtKey {get; set;} = "ec00583a-852e-4740-9972-dd14cd4a243c";    
+}
+
---
+++ Services/TokenService.cs
@@ -0,0 +1,31 @@
+namespace Blog.Services;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new System.Security.Claims.ClaimsIdentity(
+                new Claim[]
+                {
+                    new("", "")
+                }
+            ),
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), 
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
